5b1a9015b7660f50dedf694ca53e90517cf1eea4
Maina <75148285+ishakuyusufmaina@users.noreply.github.com>
2025-04-06 19:58:57 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+var playData = localStorage.getItem("playData");
+playData = playData? JSON.parse(playData) : {
+    substandardLevel: 1,
+    standardLevel: 5,
+    score: 0
+}
+async function init(db, doc, runTransaction, auth={}, onAuthStateChanged){
+    let promisedUser = new Promise(resolve=>onAuthStateChanged(auth, user=> resolve(user)))
+    let user = await promisedUser;
+    if (!user){
+        window.location.href = "signin.html";
+        return;
+    }
+    //at this level, there exists a user
+    let email = user.email;
+    let name = user.displayName;
+    let globalPlayData = await runTransaction(db, async transaction=>{
+        let doc = await transaction.get(db, doc("players", email));
+        if (!doc.exists())
+        transaction.set(doc("players", email), {
+            email: email,
+            name: name,
+            level: 1,
+            score: 0
+        });
+        return doc.data();
+    }); 
+    onAuthStateChanged(auth, user=> {if (!user) window.loaction.href = "signin.html"})
+    presentLevelSelection();
+    
+    
+    function presentLevelSelection(){
+        let level = playData.standardLevel;
+        let currLevel = playData.substandardLevel;
+        let levelSelect = document.getElementById("level");
+        levelSelect.innerHTML = "";
+        for (let i=1; i<=level; i++){
+            let option = document.createElement("option");
+            option.innerHTML = "Level " + i;
+            levelSelect.appendChild(option);
+            option.value = i;
+        }
+        levelSelect.onchange = e=> {
+            let substdLevel = levelSelect.value;
+            playData.substandardLevel = Number(substdLevel);
+            localStorage.setItem("playData", JSON.stringify(playData));
+        }
+        levelSelect.children[currLevel-1].selected = true;
+    }
+}
+
+//init();
